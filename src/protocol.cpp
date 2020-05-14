606091798fb0d976eefeacfc644ca3c4491c7304
ZioFabry <ziofabry@hotmail.com>
2020-05-14 03:23:35 +0200

    [P2P] Enforce expected outbound services

--- src/protocol.cpp
+++ src/protocol.cpp
@@ -89,7 +89,7 @@ CAddress::CAddress() : CService()
     Init();
 }
 
-CAddress::CAddress(CService ipIn, uint64_t nServicesIn) : CService(ipIn)
+CAddress::CAddress(CService ipIn, ServiceFlags nServicesIn) : CService(ipIn)
 {
     Init();
     nServices = nServicesIn;
@@ -97,7 +97,7 @@ CAddress::CAddress(CService ipIn, uint64_t nServicesIn) : CService(ipIn)
 
 void CAddress::Init()
 {
-    nServices = NODE_NETWORK;
+    nServices = NODE_NONE;
     nTime = 100000000;
 }
 
