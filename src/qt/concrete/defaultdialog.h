9d8f1894e995c45b11686ddec10b37539f37f674
ZioFabry <ziofabry@hotmail.com>
2020-05-14 03:40:13 +0200

    [GUI] Make DefaultDialog and TxDetailDialog child of FocusedDialog

--- src/qt/concrete/defaultdialog.h
+++ src/qt/concrete/defaultdialog.h
@@ -1,17 +1,17 @@
-// Copyright (c) 2019 The PIVX Core Developers
+// Copyright (c) 2019-2020 The PIVX developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef DEFAULTDIALOG_H
 #define DEFAULTDIALOG_H
 
-#include <QDialog>
+#include "qt/concrete/focuseddialog.h"
 
 namespace Ui {
 class DefaultDialog;
 }
 
-class DefaultDialog : public QDialog
+class DefaultDialog : public FocusedDialog
 {
     Q_OBJECT
 
@@ -19,7 +19,6 @@ public:
     explicit DefaultDialog(QWidget *parent = nullptr);
     ~DefaultDialog();
 
-    void showEvent(QShowEvent *event) override;
     void setText(const QString& title = "", const QString& message = "", const QString& okBtnText = "", const QString& cancelBtnText = "");
 
     bool isOk = false;
@@ -29,8 +28,6 @@ public Q_SLOTS:
 
 private:
     Ui::DefaultDialog *ui;
-protected:
-    void keyPressEvent(QKeyEvent *e) override;
 };
 
 #endif // DEFAULTDIALOG_H
